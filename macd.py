5ccaaca908d9fc9340244410d26cce1689ec9cce
Anna Berkowska <annaberkowska@protonmail.com>
2024-03-24 10:28:23 +0100

    Add choosing a range

--- macd.py
+++ macd.py
@@ -7,7 +7,7 @@ import pandas
 import matplotlib.pyplot as plt
 
 
-def prepare_data(coin_pair_name):
+def prepare_data(coin_pair_name, number_of_samples):
     extract_zip_files(f"data/zipped_data/{coin_pair_name}", f"data/unzipped_data/{coin_pair_name}")
 
     csv_files = get_sorted_csv_files(f"data/unzipped_data/{coin_pair_name}")
@@ -15,7 +15,7 @@ def prepare_data(coin_pair_name):
     add_headers(data_frames)
 
     data_frame = concatenate_data(data_frames)
-    data_frame = get_last_n_rows(data_frame, 1000)
+    data_frame = get_last_n_rows(data_frame, number_of_samples)
     data_frame = convert_to_datetime(data_frame)
 
     return data_frame
@@ -56,20 +56,6 @@ def convert_to_datetime(data_frame):
     return data_frame
 
 
-def print_close_price(data_frame, coin_pair_name):
-    plt.figure(figsize=(24, 12))
-    plt.plot(data_frame["close_time"], data_frame["close"], label="Close Price", color="cornflowerblue")
-    place_xticks(data_frame)
-    plt.xlabel("Time")
-    plt.ylabel("Price")
-    plt.gca().yaxis.set_major_formatter("${:,.0f}".format)
-    plt.title(f"Close Price Of The {coin_pair_name} Over Time")
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"plots/{coin_pair_name}/close_price_plot_.png")
-    plt.close()
-
-
 def exponential_moving_average(data_iterable, start, n):
     alpha = 2 / (n + 1)
 
@@ -108,51 +94,6 @@ def calculate_signal_line(macd_values):
     return signal_line
 
 
-def print_macd_indicator(data_frame, macd_values, signal_values, coin_pair_name, buy_points, sell_points):
-    plt.figure(figsize=(24, 12))
-    plt.plot(data_frame["close_time"], macd_values, label="MACD Line", color="mediumorchid")
-    plt.plot(data_frame["close_time"], signal_values, label="Signal Line", color="dodgerblue")
-
-    # Plot buy points
-    buy_times = [data_frame["close_time"].iloc[i] for i in buy_points]
-    buy_macd = [macd_values[i] for i in buy_points]
-    plt.scatter(buy_times, buy_macd, color="olivedrab", marker="^", label="Buy")
-
-    # Plot sell points
-    sell_times = [data_frame["close_time"].iloc[i] for i in sell_points]
-    sell_macd = [macd_values[i] for i in sell_points]
-    plt.scatter(sell_times, sell_macd, color="red", marker="v", label="Sell")
-
-    place_xticks(data_frame)
-    plt.title(f"MACD Indicator For The {coin_pair_name} Over Time")
-    plt.xlabel("Time")
-    plt.ylabel("MACD Value")
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"plots/{coin_pair_name}/macd_plot.png")
-    plt.close()
-
-
-def print_portfolio(data_frame, portfolio_values, coin_pair_name):
-    plt.figure(figsize=(24, 12))
-    plt.plot(data_frame["close_time"], portfolio_values, label="Total Value Of Portfolio", color="mediumorchid")
-    place_xticks(data_frame)
-    plt.title(f"Portfolio Value Over Time While Investing In The {coin_pair_name} Using Algorithm Based On The MACD")
-    plt.xlabel("Time")
-    plt.ylabel("Portfolio Value")
-    plt.gca().yaxis.set_major_formatter("${:,.0f}".format)
-    plt.legend()
-    plt.grid(True)
-    plt.savefig(f"plots/{coin_pair_name}/portfolio_plot.png")
-    plt.close()
-
-
-def place_xticks(data_frame):
-    date_range = pandas.date_range(start=data_frame["close_time"].min(), end=data_frame["close_time"].max(), freq="D")
-    plt.xticks(date_range, rotation=45)
-    plt.xticks(rotation=45)
-
-
 def create_dirs(coin_pair_name):
     if not os.path.exists(f"plots/{coin_pair_name}"):
         os.makedirs(f"plots/{coin_pair_name}")
@@ -187,6 +128,7 @@ def investing_algorithm(initial_funds, prices, macd_values, signal_values):
         portfolio_values.append(funds + coins * prices[i])
 
     final_funds = funds + coins * prices.iloc[-1]
+    print(buy_points)
     return final_funds, portfolio_values, buy_points, sell_points
 
 
@@ -204,10 +146,73 @@ def simple_algorithm(initial_funds, prices):
     return final_funds
 
 
+def place_xticks(data_frame):
+    date_range = pandas.date_range(start=data_frame["close_time"].min(), end=data_frame["close_time"].max(), freq="D")
+    plt.xticks(date_range, rotation=45)
+    plt.xticks(rotation=45)
+
+
+def print_close_price(data_frame, coin_pair_name, number_of_samples):
+    plt.figure(figsize=(24, 12))
+    plt.plot(data_frame["close_time"], data_frame["close"], label="Close Price", color="cornflowerblue")
+    place_xticks(data_frame)
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.gca().yaxis.set_major_formatter("${:,.0f}".format)
+    plt.title(f"Close Price Of The {coin_pair_name} Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"plots/{coin_pair_name}/close_price_plot_{number_of_samples}.svg")
+    plt.close()
+
+
+def print_macd_indicator(data_frame, macd_values, signal_values, coin_pair_name, buy_points, sell_points, number_of_samples):
+    plt.figure(figsize=(24, 12))
+    plt.plot(data_frame["close_time"], macd_values, label="MACD Line", color="mediumorchid")
+    plt.plot(data_frame["close_time"], signal_values, label="Signal Line", color="dodgerblue")
+
+    # Plot buy points
+    buy_times = [data_frame["close_time"].iloc[i] for i in buy_points]
+    buy_macd = [macd_values[i] for i in buy_points]
+    print(buy_times)
+    print(buy_macd)
+    plt.scatter(buy_times, buy_macd, color="olivedrab", marker="^", label="Buy")
+
+    # Plot sell points
+    sell_times = [data_frame["close_time"].iloc[i] for i in sell_points]
+    sell_macd = [macd_values[i] for i in sell_points]
+    plt.scatter(sell_times, sell_macd, color="red", marker="v", label="Sell")
+
+    place_xticks(data_frame)
+    plt.title(f"MACD Indicator For The {coin_pair_name} Over Time")
+    plt.xlabel("Time")
+    plt.ylabel("MACD Value")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"plots/{coin_pair_name}/macd_plot_{number_of_samples}.svg")
+    plt.close()
+
+
+def print_portfolio(data_frame, portfolio_values, coin_pair_name, number_of_samples):
+    plt.figure(figsize=(24, 12))
+    plt.plot(data_frame["close_time"], portfolio_values, label="Total Value Of Portfolio", color="mediumorchid")
+    place_xticks(data_frame)
+    plt.title(f"Portfolio Value Over Time While Investing In The {coin_pair_name} Using Algorithm Based On The MACD")
+    plt.xlabel("Time")
+    plt.ylabel("Portfolio Value")
+    plt.gca().yaxis.set_major_formatter("${:,.0f}".format)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f"plots/{coin_pair_name}/portfolio_plot_{number_of_samples}.svg")
+    plt.close()
+
+
+
 def print_information(data, initial_funds, final_funds, simple_final_funds):
     final_change = final_funds / initial_funds * 100
     simple_final_change = simple_final_funds / initial_funds * 100
 
+    pandas.set_option('display.max_rows', None)
     print(data)
 
     print(f"In the end, funds invested at the beginning of the value of ${initial_funds:,.2f}, "
@@ -231,17 +236,19 @@ def print_information(data, initial_funds, final_funds, simple_final_funds):
 if __name__ == "__main__":
     coin_pair_name = str(sys.argv[1])
     initial_funds = float(sys.argv[2])
+    number_of_samples = int(sys.argv[3])
 
     create_dirs(coin_pair_name)
 
-    data = prepare_data(coin_pair_name)
+    data = prepare_data(coin_pair_name, number_of_samples)
     macd = calculate_macd_line(data)
     signal = calculate_signal_line(macd)
     final_funds, portfolio, buy_points, sell_points = investing_algorithm(initial_funds, data["close"], macd, signal)
     simple_final_funds = simple_algorithm(initial_funds, data["close"])
 
-    print_close_price(data, coin_pair_name)
-    print_macd_indicator(data, macd, signal, coin_pair_name, buy_points, sell_points)
-    print_portfolio(data, portfolio, coin_pair_name)
+    print_close_price(data, coin_pair_name, number_of_samples)
+    print(data)
+    print_macd_indicator(data, macd, signal, coin_pair_name, buy_points, sell_points, number_of_samples)
+    print_portfolio(data, portfolio, coin_pair_name, number_of_samples)
 
     print_information(data, initial_funds, final_funds, simple_final_funds)
